c8d62cf00d7abaff2fa2e6523719f928f56c50ee
CesarBeda <cesarbedacandido@hotmail.com>
2024-05-27 22:15:01 -0300

    JavaScript CFB Cursos - aula 08
    Operadores Bitwise, exemplos.

---
+++ Estudos/CFB-Cursos/JavaScript/aula08/aula08.js
@@ -0,0 +1,39 @@
+// Operadores Bitwise
+
+/* &, |, ^*/
+
+let n1=11;
+let n2=14;
+
+let res = n1 & n2; // = a 10 em binário
+
+console.log(res);
+
+n1=10;
+n2=11;
+
+res = n1 | n2; // = a 11 em binário
+
+console.log(res);
+
+n1=13;
+n2=14;
+
+res = n1 ^ n2; // = a 3 em binário
+
+console.log(res);
+
+n1=25;
+
+res = n1 << 1; // desloca 1 bit para esquerda
+
+console.log(res);
+
+n1=30;
+
+res = n1 >> 1; // desloca 1 bit para esquerda
+
+console.log(res);
+
+//assistir de novo em outro momento:
+/* https://youtu.be/JbGf_hd6g_Q?list=PLx4x_zx8csUg_AxxbVWHEyAJ6cBdsYc0T */
