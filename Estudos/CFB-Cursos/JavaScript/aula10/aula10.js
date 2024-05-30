d3e9ea2a1c42b456cd14de96b2bb98c533a869b2
CesarBeda <cesarbedacandido@hotmail.com>
2024-05-29 21:46:22 -0300

    aula 10 CFB Cursos javascript
    Operador Ternário

---
+++ Estudos/CFB-Cursos/JavaScript/aula10/aula10.js
@@ -0,0 +1,19 @@
+//Operador ternário
+
+//?:
+// condition ? return true : return false
+
+let num=11;
+let res = !(num%2) ? 'par': 'impar';
+
+console.log(res);
+
+//0-false
+//1 - true
+
+
+let n1 = 7;
+let n2 = 3;
+let r = n1 > n2 ? 'Maior' : 'Menor'
+
+console.log(r); 
