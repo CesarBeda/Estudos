cf2c1c6ec85d9424f32744fdcd000e84e1d63886
CesarBeda <cesarbedacandido@hotmail.com>
2024-08-26 22:50:16 -0300

    Python Santander Open Academy
    Criação do repositório e primeiro código do curso introdutório de python do Santander Open Academy

---
+++ Estudos/Santander Open Academy/Introdução Python/ola_mundo.py
@@ -0,0 +1,18 @@
+print ("Olá Mundo")
+
+# Este é um comentário de uma única linha.
+
+"""
+Este é um comentário
+de várias linhas
+"""
+
+
+num = 1
+
+if num==1:
+    #Bloco de código se a condição for verdadeira
+    print('No Python, a indentação (espaços ou tabulações no início de uma linha) é utilizada para delimitar blocos de código.')
+else:
+    # Bloco de código se a condição for falsa
+    print("Diferente de outras linguagens que utilizam chaves ou palavras-chave, o Python utiliza a indentação para determinar o escopo das declarações.")
